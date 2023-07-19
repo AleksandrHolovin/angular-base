ead32220141c30bece883cf8fb6f2d4ba4875a35
Oleksandr Holovin <emanuel@conicapps.com>
2023-07-19 22:24:54 +0300

    Added base project

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Observable, catchError, tap, throwError } from "rxjs";
+import { IProduct } from "../models/product";
+import { ErrorService } from "./error.service";
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ProductService {
+    constructor(
+        private http: HttpClient,
+        private errorService: ErrorService
+    ) { }
+
+    products: IProduct[] = []
+
+    getAll(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
+            params: new HttpParams({
+                fromObject: { limit: 5 }
+            })
+        }).pipe(
+            catchError(this.errorHandler.bind(this)),
+            tap(products => this.products = products)
+        )
+    }
+
+    create(product: IProduct): Observable<IProduct> {
+        return this.http.post<IProduct>('https://fakestoreapi.com/products', product)
+            .pipe(
+                tap(product => this.products.push(product))
+            )
+    }
+
+    private errorHandler(error: HttpErrorResponse) {
+        this.errorService.handle(error.message)
+        return throwError(() => error.message)
+    }
+};
